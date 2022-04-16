1f3ef7531cc780c3de5ccf9ad88a4c9f7b5369af
30671 <3067132996@qq.com>
2022-04-16 10:36:39 +0800

    2022/3/25

---
+++ src/main/java/controller/borrow/QueryBookController.java
@@ -0,0 +1,17 @@
+package controller.borrow;
+
+import controller.readertype.HelloServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * # -*- coding:utf-8 -*- #
+ * 作者:30671
+ * 日期:2022年04月16日10时36分
+ */
+@WebServlet(name = "LendBookController", urlPatterns = {"/LendBookController"})
+public class QueryBookController extends HelloServlet {
+
+
+
+}
---
+++ src/main/java/controller/borrow/ReturnBookController.java
@@ -0,0 +1,12 @@
+package controller.borrow;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * # -*- coding:utf-8 -*- #
+ * 作者:30671
+ * 日期:2022年04月16日10时12分
+ */
+@WebServlet(name = "ReturnBookController", urlPatterns = {"/ReturnBookController"})
+public class ReturnBookController {
+}
