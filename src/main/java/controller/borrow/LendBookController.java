4c76cea74b542a043c763d6b566ff466ab32959d
30671 <3067132996@qq.com>
2022-04-16 11:19:41 +0800

    2022/3/25

--- src/main/java/controller/borrow/LendBookController.java
+++ src/main/java/controller/borrow/LendBookController.java
@@ -1,6 +1,19 @@
 package controller.borrow;
 
+import pojo.Book;
+import pojo.Borrow;
+import service.book.BookService;
+import service.book.BookServiceImpl;
+import service.borrow.BorrowService;
+import service.borrow.BorrowServiceImpl;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * # -*- coding:utf-8 -*- #
@@ -8,5 +21,17 @@ import javax.servlet.annotation.WebServlet;
  * 日期:2022年04月16日10时12分
  */
 @WebServlet(name = "LendBookController", urlPatterns = {"/LendBookController"})
-public class LendBookController {
+public class LendBookController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    public List<Boolean> LendBooks(List<Borrow> borrows) {
+
+        BorrowService borrowService = new BorrowServiceImpl();
+
+        return borrowService.lendBorrows(borrows);
+    }
+
 }
